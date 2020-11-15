815dd37b939636a122bdc91f4863a73a875b237b
ax <apetryga@gmail.com>
2020-11-14 22:28:09 -0500

    saturday commit

--- script.js
+++ script.js
@@ -1,73 +1,3 @@
-    /** FROM https://ourcodeworld.com/articles/read/185/how-to-get-the-pixel-color-from-a-canvas-on-click-or-mouse-event-with-javascript**/
-
-    const canvas = $('#canvas')[0]
-
-    function getElementPosition(obj) {
-        let curleft = 0, curtop = 0;
-        if (obj.offsetParent) {
-            do {
-                curleft += obj.offsetLeft;
-                curtop += obj.offsetTop;
-            } while (obj = obj.offsetParent);
-            return { x: curleft, y: curtop };
-        }
-        return undefined;
-    }
-
-    function getEventLocation(element, event) {
-        const pos = getElementPosition(element);
-
-        return {
-            x: (event.pageX - pos.x),
-            y: (event.pageY - pos.y)
-        };
-    }
-
-    function rgbToHex(r, g, b) {
-        if (r > 255 || g > 255 || b > 255)
-            throw "Invalid color component";
-        return ((r << 16) | (g << 8) | b).toString(16);
-    }
-
-    function drawImageFromWebUrl(sourceurl) {
-        const img = new Image();
-
-        img.addEventListener("load", function () {
-            // The image can be drawn from any source
-            canvas.getContext("2d").drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
-
-        });
-
-        img.setAttribute("src", sourceurl);
-    }
-    // Draw a base64 image because this is a fiddle, and if we try with an image from URL we'll get tainted canvas error
-    // Read more about here : http://ourcodeworld.com/articles/read/182/the-canvas-has-been-tainted-by-cross-origin-data-and-tainted-canvases-may-not-be-exported
-    let img = document.getElementById('img');
-    canvas.getContext("2d").drawImage(img, 10, 10);
-    canvas.addEventListener("click", function (e) {
-        const eventLocation = getEventLocation(this, e);
-        const coord = "x=" + eventLocation.x + ", y=" + eventLocation.y;
-
-        // Get the data of the pixel according to the location generate by the getEventLocation function
-        const context = this.getContext('2d');
-        const pixelData = context.getImageData(eventLocation.x, eventLocation.y, 1, 1).data;
-
-        // If transparency on the image
-        if ((pixelData[0] == 0) && (pixelData[1] == 0) && (pixelData[2] == 0) && (pixelData[3] == 0)) {
-            console.log("it is transparent");
-        } else {
-            console.log("It is not transparent");
-        }
-
-        // var hex = "#" + ("000000" + rgbToHex(pixelData[0], pixelData[1], pixelData[2])).slice(-6);
-
-        // Draw the color and coordinates.
-        // document.getElementById("status").innerHTML = coord;
-        // document.getElementById("color").style.backgroundColor = hex;
-    }, false);
-
-
-
 const myApp = {};
 //create an array of all available background images
 myApp.backgroundImages = [
@@ -122,17 +52,17 @@ myApp.backgroundImages = [
 ]
 myApp.skylineImages = [
     {
-        name: 'toronto',
-        url: './assets/toronto.png',
-        alt: 'black Toronto skyline',
-    },
-    {
         name: 'vancouver',
         url: './assets/vancouver.png',
         alt: 'black Vancouver skyline',
     },
+    {
+        name: 'toronto',
+        url: './assets/toronto.png',
+        alt: 'black Toronto skyline',
+    },
 ]
-
+console.log(myApp.skylineImages);
 myApp.init = () => {
 
     //add a function to make the background move with mouse position
@@ -158,7 +88,7 @@ myApp.init = () => {
     }
 
     //get user's input - event listener on button click
-    $('button').on('click', function (event) {
+    $('button').not('#skyline-btn').on('click', function (event) {
         event.preventDefault();
         //get button values after user click on it - clouds, aurora, yellow etc - and store them in a variable
         const userChoice = this.id;
@@ -184,50 +114,53 @@ myApp.init = () => {
 
 
 
+
+    
+    myApp.currentSkyline = 0;
+    $('#skyline-btn').click(function () {
+    // add event listener on the skyline button that will fire off once the user clicks it
+    // display a next skyline image on a page
+    let skylineURL = myApp.skylineImages[myApp.currentSkyline].url;
+    let skylineAlt = myApp.skylineImages[myApp.currentSkyline].alt;
+    $('#skyline-img').attr('src',`${skylineURL}`).attr('alt',`${skylineAlt}`);
+    myApp.currentSkyline++;
+    if (myApp.currentSkyline >= myApp.skylineImages.length) {
+        myApp.currentSkyline = 0;
+    }
+})
+
+
+myApp.backgroundUpdater = function(){
+    // get urls of all background images 
+    let imageURL = myApp.backgroundImages[myApp.currentBackground].url;
+    
+    //go through all the available background images and display them when the user clicks - one after another 
+    $('.image').css('background-image', `url(${imageURL})`);
+    myApp.currentBackground++;
+    if (myApp.currentBackground >= myApp.backgroundImages.length) {
+        myApp.currentBackground = 0;
+    }
+    
+    //add tabindex
+    $(this).each(function (i) {
+        $(this).attr('tabindex', i + 1)
+    });
+}
+
+
     //add event listener on the background image that will fire off once the user clicks the background image
     myApp.currentBackground = 0;
 
-    $(".image").click(function () {
-
-        // get urls of all background images 
-        let imageURL = myApp.backgroundImages[myApp.currentBackground].url;
-
-        //loop through all the available background images and display them when the user clicks - one after another 
-        $('.image').css('background-image', `url(${imageURL})`);
-        myApp.currentBackground++;
-        if (myApp.currentBackground >= myApp.backgroundImages.length) {
-            myApp.currentBackground = 0;
-        }
-        //add animation - doesn't work because same div - add if skyline canvas is fixed
-        // $('.image').not('img').fadeOut( "slow", function() {
-        //     $('.image').not('img').fadeIn('slow');
-        // });
-
-        //add tabindex
-        $(this).each(function (i) {
-            $(this).attr('tabindex', i + 1)
-        });
+    $(".image").on('click', function () {
+        myApp.backgroundUpdater();
     });
 
-    // add event listener on the skyline that will fire off once the user clicks it
-    // display a next skyline image on a page
-
-    // $('img').on('click', function () {
-    //     const imageUrl = "./assets/vancouver.png";
-    //     $('#toronto').attr('src', imageUrl, 'alt', 'vancouver')
-    //     return false
-    // })
-
-
-
-
-
-
-
-
-
-
-
+    $(".image").on('keydown', function (e) {
+        let code = e.keyCode || e.which;
+        if (code == 32) {
+        myApp.backgroundUpdater();
+        }
+    });
 
 }
 $(function () {
